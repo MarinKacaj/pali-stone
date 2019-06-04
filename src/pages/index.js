f8511e5021a8cb389aebc623e798b58f58070c3c
Marin Kacaj <marink@backbase.com>
2019-06-04 22:46:45 +0200

    integrate with formspree instead of netlify forms

--- src/pages/index.js
+++ src/pages/index.js
@@ -121,18 +121,18 @@ class HomeIndex extends React.Component {
                         <h2>Contact</h2>
                         <div className="row">
                             <div className="8u 12u$(small)">
-                                <form name="contact" method="POST" data-netlify="true">
+                                <form name="contact" method="POST" action="https://formspree.io/m93_vkacaj@yahoo.com">
                                     <div className="row uniform 50%">
                                         <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Prenom" /></div>
                                         <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
                                         <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
                                     </div>
                                     <div className="row uniform">
-				        <ul className="actions">
+                                        <ul className="actions">
                                             <li><input type="submit" value="Envoyer" /></li>
                                         </ul>
-				    </div>
-				</form>
+                                    </div>
+                                </form>
                             </div>
                             <div className="4u 12u$(small)">
                                 <ul className="labeled-icons">
